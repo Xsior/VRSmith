506809f1ec5d6580a405daba33ec94b4206bbc09
Xsior <xsior95@gmail.com>
2018-03-20 13:48:19 +0100

    Initial commit

---
+++ VRSmith/Assets/Scripts/ControlTest.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTest : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        collidingObject = col.gameObject;
+    }
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    // 3
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<FixedJoint>())
+        {
+            // 2
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // 3
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        // 4
+        objectInHand = null;
+    }
+
+
+    void Update()
+    {
+        if (Controller.GetAxis() != Vector2.zero)
+        {
+            Debug.Log(gameObject.name + Controller.GetAxis());
+        }
+
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Press");
+        }
+
+        // 5
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Release");
+        }
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // 2
+        if (Controller.GetHairTriggerUp())
+        {
+            Debug.Log(gameObject.name + " Trigger Release");
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+
+
+    }
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+}
